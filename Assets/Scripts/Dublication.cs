4a8077c1a1c14a1da79c35ddf6ee3d6710bec23e
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2021-12-12 19:32:24 +0400

    New shop system added

--- Assets/Scripts/Dublication.cs
+++ Assets/Scripts/Dublication.cs
@@ -7,36 +7,6 @@ using TMPro;
 
 public class Dublication : MonoBehaviour
 {
-    public GameObject Slot;
-    public List<Items> Items;
-    void Start()
-    {
-        CallMarket();
-    }
-
-    void CallMarket()
-    {
-        for (int i = 0; i < Items.Count; i++)
-        {
-            GameObject IncreasedSlot = Instantiate(Slot, transform);
-            IncreasedSlot.GetComponent<Image>().sprite = Items[i].Sprite;
-            IncreasedSlot.GetComponent<TMP_Text>().text = Items[i].ShopItemName.text;
-        }
-    }
+    
 }
 
-[System.Serializable]
-public class Items
-{
-    public TMP_Text ShopItemName;
-    public string Name;
-    public Sprite Sprite;
-
-    public Items(TMP_Text shopItemName, string name, Sprite sprite)
-    {
-        Name = name;
-        Sprite = sprite;
-        ShopItemName = shopItemName;
-        shopItemName.text = name;
-    }
-}
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class ShopManager : MonoBehaviour
 {
-    public void IncreaseSpeed()
+    public int coins;
+    public TMP_Text coinUI;
+    public ShopItemSO[] shopItemSO;
+    public ShopTemplate[] shopPanels;
+
+    void Start()
     {
-        PlayerManager.forwardSpeed += 1;
+        coinUI.text = "Coins: " + coins.ToString();
+        LoadPanels();
+    }
+
+    public void AddCoins()
+    {
+        coins++;
+        coinUI.text = "Coins: " + coins.ToString();
+        //CheckPurchaseable();
+    }
+
+    public void LoadPanels()
+    {
+        for (int i = 0; i < shopItemSO.Length; i++)
+        {
+            shopPanels[i].titleText.text = shopItemSO[i].title;
+            shopPanels[i].descriptionText.text = shopItemSO[i].description;
+            shopPanels[i].costTxt.text = "Coins: " + shopItemSO[i].baseCoin.ToString();
+        }
     }
 }
